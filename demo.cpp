d1ae2e1a1c9c83b9cb25f20e9744b68e25c37999
Lim Ming Yean <mingyean@hotmail.com>
2018-12-05 17:07:52 +0800

    Added some comments and an additional example

--- demo.cpp
+++ demo.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
 #include "matrix.hpp"
 
+// The matrices ZMatrix and IMatrix are declared in the namespace "matrix"
+// IMatrix is a specialization of ZMatrix
 using namespace matrix;
 
 int main() {
     // Declare matrix using initializer list
+    // Matrices can be declared const
     const IMatrix<3, 3> mat {
         1, 2, 3,
         4, 5, 6,
         7, 8, 9
     };
 
-    // Declaring column vector using initializer list
+    // Declare column vector using initializer list
     IVectorC<3> vec {
         1,
         2,
         3
     };
 
-    // Declaring row vector using initializer list
+    // Declare row vector using initializer list
     IVectorR<3> rvec {
         1, 2, 3
     };
 
-    // Get entry
+    // Get entry using ZMatrix::at(size_t, size_t)
+    // Can also use mat[1][2] for non-const matrices
     std::cout << "mat[1][2]=" << mat.at(1, 2) << std::endl;
 
-    // Printing matrices
+    // Printing matrices / putting them to std::ostream
     std::cout << "mat=\n" << mat << std::endl;
     std::cout << "vec=\n" << vec << std::endl;
     std::cout << "rvec=\n" << rvec << std::endl;
 
-    // Negating a matrix
+    // Matrix negation
     std::cout << "-mat=\n" << -mat << std::endl;
 
     // Scalar multiplication
@@ -42,6 +46,10 @@ int main() {
     std::cout << "mat*vec=\n" << mat * vec << std::endl;
     std::cout << "vec*rvec=\n" << vec * rvec << std::endl;
     std::cout << "rvec*vec=\n" << rvec * vec << std::endl;
+
+    // Other types can be used, such as double
+    ZMatrix<double, 2, 2> doubles {1.1, -0.7, 0.4, 1.2};
+    std::cout << doubles << std::endl;
 
     return 0;
 }
